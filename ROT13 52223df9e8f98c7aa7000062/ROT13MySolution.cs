4476330bbb8900e5da7566a52a59e4f112f6a164
Joey Morit <z.jmorit@gmail.com>
2022-04-11 00:30:25 +0200

    Added ROT13 solutions

---
+++ ROT13 52223df9e8f98c7aa7000062/ROT13MySolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeWarsSolutions {
+    public class ROT13MySolution {
+        //https://www.codewars.com/kata/reviews/599a2180a48343e7b900172d/groups/62535684c522ea0001d72d83
+        public static string Rot13(string input) {
+            string result = "";
+            foreach(char c in input) {
+                if (char.IsUpper(c)) {
+                    result += (c + 13 > 90 ? Convert.ToChar(c - 13) : Convert.ToChar(c + 13));
+                    continue;
+                }
+                if (char.IsLower(c)) {
+                    result += (c + 13 > 122 ? Convert.ToChar(c - 13) : Convert.ToChar(c + 13));
+                    continue;
+                }
+                result += c;
+            }
+            return result;
+        }
+    }
+}
---
+++ ROT13 52223df9e8f98c7aa7000062/ROT13TopSolution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+public class Kata {
+    //https://www.codewars.com/kata/reviews/599a2180a48343e7b900172d/groups/599a83857781220eb100014e
+    public static string Rot13(string input) {
+        var s1 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+        var s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        return string.Join("", input.Select(x => char.IsLetter(x) ? s1[s2.IndexOf(x)] : x));
+    }
+}
